42fee66790a3c55321c3edec1000df1b5755515a
yongwoon <yonyon.japan@gmail.com>
2020-12-10 09:51:57 +0000

    0.4 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// How to execute ?
+// yarn start
+const name = "yongwoon", age = 30, gender = "mail";
+// const sayHi = (name, age, gender) => {
+//   console.log(`Hello ${name}, your are ${age}, you are a ${gender}`)
+// }
+// sayHi(name, age, gender)
+// optional gender?
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, your are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello")
+// How to execute ?
+// yarn start
+
+const sayHi = (name: string, age: number, gender: string) => {
+  console.log(`Hello ${name}, your are ${age}, you are a ${gender}`)
+}
+
+sayHi("yongwoon", 30, "male")
+
+
+// export 하므로서 module 로 사용가능하다.
+export {};
