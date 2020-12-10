28dd259f3ee700789277b1b7c4ed70b6cf8c2961
yongwoon <yonyon.japan@gmail.com>
2020-12-10 19:42:41 +0900

    0.8 Blockchain Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,15 @@
 "use strict";
-// How to execute ?
-// yarn start
 Object.defineProperty(exports, "__esModule", { value: true });
-// interface 는 컴파일 되지 않는다
-// interface 를 js 파일(커뮤ㅏ일된 파일)에 넣고 싶을 땐, class를 사용한다.
-// interface Human {
-//   name: string,
-//   age: number,
-//   gender: string
-// }
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const person = new Human("yongwoon kim", 30, "male");
-const sayHi = (persion) => {
-    return `Hello ${persion.name}, your are ${persion.age}, you are a ${persion.gender}`;
-};
-console.log(sayHi(person));
+const genesisBlock = new Block(0, "24tq5w34", "", "hello", 1234567);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
-// How to execute ?
-// yarn start
+class Block{
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-// interface 는 컴파일 되지 않는다
-// interface 를 js 파일(커뮤ㅏ일된 파일)에 넣고 싶을 땐, class를 사용한다.
-// interface Human {
-//   name: string,
-//   age: number,
-//   gender: string
-// }
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number,
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const person = new Human("yongwoon kim", 30, "male")
+const genesisBlock: Block = new Block(0, "24tq5w34", "", "hello", 1234567)
 
+let blockchain: [Block] = [genesisBlock]
 
-const sayHi = (persion: Human): string => {
-  return `Hello ${persion.name}, your are ${persion.age}, you are a ${persion.gender}`;
-}
-
-console.log(sayHi(person))
-
-
+console.log(blockchain)
 // export 하므로서 module 로 사용가능하다.
 export {};
