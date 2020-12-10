a8e70ceb631b40b087643a6261b9af8bf19e417c
yongwoon <yonyon.japan@gmail.com>
2020-12-10 19:36:07 +0900

    0.7 Classes on Typescript part One

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,21 @@
 // How to execute ?
 // yarn start
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "yongwoon kim",
-    age: 30,
-    gender: "male"
-};
+// interface 는 컴파일 되지 않는다
+// interface 를 js 파일(커뮤ㅏ일된 파일)에 넣고 싶을 땐, class를 사용한다.
+// interface Human {
+//   name: string,
+//   age: number,
+//   gender: string
+// }
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const person = new Human("yongwoon kim", 30, "male");
 const sayHi = (persion) => {
     return `Hello ${persion.name}, your are ${persion.age}, you are a ${persion.gender}`;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 // How to execute ?
 // yarn start
 
-interface Human {
-  name: string,
-  age: number,
-  gender: string
+// interface 는 컴파일 되지 않는다
+// interface 를 js 파일(커뮤ㅏ일된 파일)에 넣고 싶을 땐, class를 사용한다.
+// interface Human {
+//   name: string,
+//   age: number,
+//   gender: string
+// }
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person: Human = {
-  name: "yongwoon kim",
-  age: 30,
-  gender: "male"
-};
+const person = new Human("yongwoon kim", 30, "male")
+
 
 const sayHi = (persion: Human): string => {
   return `Hello ${persion.name}, your are ${persion.age}, you are a ${persion.gender}`;
